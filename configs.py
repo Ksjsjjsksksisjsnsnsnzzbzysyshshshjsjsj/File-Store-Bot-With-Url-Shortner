f60ea691e22274862d7d357ceff20b66840cb284
Ksjsjjsksksisjsnsnsnzzbzysyshshshjsjsj <koushalprasad35@gmail.com>
2024-07-13 16:44:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,28 +1,28 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "9619481"))
-  API_HASH = os.environ.get("API_HASH", "10effb30531c66d27b90f1e07f6bd071")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6725760034:AAEHsb0N2E93k8JPlDohvlYJmIEGFIdxCeM")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Kmoviestore_bot")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002011038623"))
+  API_ID = int(os.environ.get("API_ID", "23580609"))
+  API_HASH = os.environ.get("API_HASH", "5e191a05983f2f63986e92bcc4f94835")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7085463016:AAGAcPG-pKHe4ZJXqHXJM7IBQZwvXRD8euU")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "publicfilestorebot_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002249026695"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "api.shareus.io")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "X8KgVEykIqhQIXefXcmrJb9aSPt2")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6624919731"))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "7392904577"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://fffworld57:Or97XLuv6P6GvtFZ@cluster0.u34tcer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002115836678"))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002216650392"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
-This is a Permanent private bot. 
-𝙳𝚘 𝙽𝚘𝚝 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝙱𝚘𝚝, 𝙸𝚏 𝚢𝚘𝚞 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝙱𝚘𝚝 𝚢𝚘𝚞 𝚠𝚒𝚕𝚕 𝙱𝚎 𝙱𝚊𝚗𝚗𝚎𝚍 𝙱𝚎𝚌𝚊𝚞𝚜𝚎 𝚝𝚑𝚒𝚜 𝚒𝚜 𝚊 𝙿𝚛𝚒𝚟𝚊𝚝𝚎 𝙱𝚘𝚝 , 𝚗𝚘𝚝 𝚊 𝚙𝚞𝚋𝚕𝚒𝚌 𝙱𝚘𝚝.
+This is a Permanent Public file Store bot. 
+𝗣𝗹𝗲𝗮𝘀𝗲 𝗗𝗼𝗻'𝘁 𝗨𝗽𝗹𝗼𝗮𝗱 𝟭𝟴+ 𝗩𝗶𝗱𝗲𝗼𝘀 𝗜𝗳 𝘆𝗼𝘂 𝘂𝗽𝗹𝗼𝗮𝗱 𝘁𝗵𝗲𝗻 𝘆𝗼𝘂 𝗯𝗮𝗻𝗻𝗲𝗱 💀.
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [private Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [This is Public File 📁 Store Bot.](https://t.me/{BOT_USERNAME})
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
