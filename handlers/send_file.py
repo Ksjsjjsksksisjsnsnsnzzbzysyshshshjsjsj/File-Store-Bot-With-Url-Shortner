9862be7a41689b4204361f2f97d964dabcea6af0
Ksjsjjsksksisjsnsnsnzzbzysyshshshjsjsj <koushalprasad35@gmail.com>
2024-07-13 17:11:53 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,7 +11,7 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"Files will be deleted in 𝟲𝟬 minutes to avoid copyright issues. Please forward and save them. ",
+            f"Files will be deleted in 𝟲𝟬 minutes to avoid copyright issues. Please forward and save them.ﾠﾠJoin Moneyﾠ💰ﾠhttps://t.me/+MxH5jcG5cek5OWNl. ",
             disable_web_page_preview=True,
             quote=True
         )
